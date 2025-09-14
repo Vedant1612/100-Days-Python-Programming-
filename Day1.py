2af81ca1ddd871bb7d9ff20c9e7ac14a0f200d03
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2025-09-14 15:27:59 +0530

    Day_1

---
+++ Day1.py
@@ -0,0 +1,14 @@
+# print("Hello World!\nHello World!\nHello World!")
+# print("Hello" +" "+ "Vedant")
+
+# print("Hello " + input("What is your name? ") + "!")
+
+# name  = input("What is your name? ")
+# print("Hello " + name + "!" + " Your name contains " + str(len(name)) + " characters.")
+# print(len(name))
+
+# Brand Name Project:
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
